941e785ba0041fd16521362fae1fd74086d3c677
Albert Brox III <albert@exypno.tech>
2017-06-02 18:48:03 -0400

    typo fix

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ SAMPLING_RATE = 44100
 DURATION = 1.0
 FREQ = 432.0
 
-samples = (np.sin(2 * np.pi * np.arrange(SAMPLING_RATE * DURATION) * FREQ / SAMPLING_RATE))\
+samples = (np.sin(2 * np.pi * np.arange(SAMPLING_RATE * DURATION) * FREQ / SAMPLING_RATE))\
     .astype(np.float32)
 
 stream = p.open(format=pyaudio.paFloat32,
