d6500e0a89332166dad7ce6cd985830bc4fdbd24
Albert Brox III <albert@exypno.tech>
2017-06-02 18:49:44 -0400

    typo fix + added duration input

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ p = pyaudio.PyAudio()
 
 VOL = 0.5 ## in range [0.0, 1.0]
 SAMPLING_RATE = 44100
-DURATION = 1.0
 FREQ = 432.0
+DURATION = int(input("Duration? "))
 
 samples = (np.sin(2 * np.pi * np.arange(SAMPLING_RATE * DURATION) * FREQ / SAMPLING_RATE))\
     .astype(np.float32)
@@ -17,7 +17,7 @@ stream = p.open(format=pyaudio.paFloat32,
     rate=SAMPLING_RATE,
     output=True)
 
-stream.write(VOL * SAMPLES)
+stream.write(VOL * samples)
 stream.stop_stream()
 stream.close()
 
