08891f448c756e1581a3821383579386bf278fc5
Albert Brox III <albert@exypno.tech>
2017-06-02 20:24:20 -0400

    committing prior to totally rewriting in order to have in git history

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pyaudio
+import struct
 import numpy as np
 
 
@@ -13,11 +14,25 @@ samples = (np.sin(2 * np.pi * np.arange(SAMPLING_RATE * DURATION) * FREQ / SAMPL
     .astype(np.float32)
 
 stream = p.open(format=pyaudio.paFloat32,
-    channels=1, 
+    channels=2, 
     rate=SAMPLING_RATE,
     output=True)
 
-stream.write(VOL * samples)
+left_chunk = (np.sin(np.arange(SAMPLING_RATE * DURATION) * FREQ / SAMPLING_RATE)).astype(np.float32)
+right_chunk = (np.sin(np.arange(SAMPLING_RATE * DURATION) * (FREQ + 10) / SAMPLING_RATE)).astype(np.float32)
+
+# print("left_chunk:", left_chunk)
+# print("right_chunk:", right_chunk)
+
+# assert len(left_chunk) == len(right_chunk)
+# for i in range(len(left_chunk)):
+#     stream_data = struct.pack("2f", left_chunk[i] * VOL, right_chunk[i] * VOL)
+# 
+# print("stream_data:", stream_data)
+
+stereo = pack('<' + 2 * SAMPLING_RATE * 'l', )
+
+stream.write(stereo)
 stream.stop_stream()
 stream.close()
 
